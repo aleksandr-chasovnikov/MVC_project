47c96c373e2a3b68fffb9e1d40434ece40bc35d5
k241285@mail.ru <aleksandr.chasovnikov@gmail.com>
2017-03-07 18:57:09 +0300

    1

--- components/Router.php
+++ components/Router.php
@@ -10,9 +10,44 @@ class Router
         $this->routes = include($routesPath);
     }
     
+    /**
+     * Return request string
+     */
+    private function getURI()
+    {
+        if (!empty($_SERVER['REQUEST_URI'])) {
+            return trim($_SERVER['REQUEST_URI'], '/');
+        }
+    }
+
     public function run()
     {
-        print_r($this->routes);
-        echo 'Class Router method run';
+        // Получить строку запроса
+        $uri = $this->getURI();
+        echo $uri;
+        
+        // Проверить наличие такого запроса в routes.php
+        foreach ($this->routes as $uriPattern => $path) {
+            
+            // Сравниваем $uriPattern и $uri
+            if (preg_match("~$uriPattern~", $uri)) {
+               
+                // Определить какой контроллер
+                // и action обрабатывают запрос
+                
+                $segments = explode('/', $path);
+                $cotrollerName = array_shift($segments).'Controller';
+                $cotrollerName = ucfirst($cotrollerName);
+                echo $cotrollerName;
+                
+            }
+        }
+        
+        // Если есть совпадение, определить какой контроллер
+        // и action обрабатывают запрос
+        
+        // Подключить файл класса-контроллера
+        
+        // Создать объект, вызвать метод (т.е. action_
     }
 }
